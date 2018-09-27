05123059efb0cf13a626350aac8a6a5411c0f964
Yaqub-Ibrahim <37085705+Yaqub-Ibrahim@users.noreply.github.com>
2018-09-27 13:24:29 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -1,21 +1,26 @@
 function simpleOperators (numberA, numberB) {
-  let adding = numberA + numberB
-  let subtract = numberA - numberB
-  let multiply = numberA * numberB
-  let divide = numberA / numberB
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let multiply = numberA * numberB;
+  let divide = numberA / numberB;
   console.log(adding);
   console.log(subtract);
   console.log(multiply);
   console.log(divide);
 }
 
+//SkipCount by ____ for ____ starting at _____
+function skipCounting (skipCount, times, startingNumber,){
+  let number = startingNumber-skipCount;
+  for (let i=1; i<=times; i++){
+    number+= skipCount;
+    console.log(number);
+  }
+
+}
+
 let number = [4, 7, 9, 11];
 
 simpleOperators (number[0], number[1]);
 simpleOperators (number[2], number[3]);
-
-let skipCount = 0 //skipCount by 2's
-for (let i=1; i<=5; i++){
-  skipCount += 2;
-  console.log(skipCount)
-}
+skipCounting(15, 10, 0);
