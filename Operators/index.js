43ef2c162b36c8bd3055b05ebc2457eabfa4e197
Yaqub-Ibrahim <37085705+Yaqub-Ibrahim@users.noreply.github.com>
2018-10-01 14:08:22 -0600

    update

--- Operators/index.js
+++ Operators/index.js
@@ -1,26 +1,40 @@
-function simpleOperators (numberA, numberB) {
+function simpleOperators (numberA, numberB){
   let adding = numberA + numberB;
   let subtract = numberA - numberB;
-  let multiply = numberA * numberB;
+  let mutliply = numberA * numberB;
   let divide = numberA / numberB;
   console.log(adding);
   console.log(subtract);
-  console.log(multiply);
+  console.log(mutliply);
   console.log(divide);
 }
-
-//SkipCount by ____ for ____ starting at _____
-function skipCounting (skipCount, times, startingNumber,){
-  let number = startingNumber-skipCount;
+//Skipcount by ___ for ___ times starting at ___
+function skipCounting (skipCount, times, startingNumber){
+  let number = startingNumber - skipCount;
   for (let i=1; i<=times; i++){
-    number+= skipCount;
+    number += skipCount;
     console.log(number);
   }
-
 }
 
-let number = [4, 7, 9, 11];
+//simpleComparision () {}
+function howManyTimes (numberStart, numberEnd, skipCount){
+  while (numberStart < numberEnd) {
+    console.log(numberStart += skipCount);
+  }
+}
 
-simpleOperators (number[0], number[1]);
-simpleOperators (number[2], number[3]);
-skipCounting(15, 10, 0);
+//Truth vs. Falsy Example
+console.log(1==1); //Returns True
+console.log('1S'==1);  //Returns True
+console.log('1'===1);  //Returns false
+
+
+//shrink the code
+
+let number = [4, 7, 13, 99, 0, 100, 5];
+//simpleOperators (number[0], number[1]);
+//simpleOperators (number[2], number[3]);
+//skipCounting (3, 6, 113);
+//skipCounting (8, 11, -351);
+howManyTimes (number[4], number[5], number[6]);
