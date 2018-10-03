9943102cf9b4c1d4a71ec5900b65dfefb80cba2f
Yaqub-Ibrahim <37085705+Yaqub-Ibrahim@users.noreply.github.com>
2018-10-03 14:09:47 -0600

    update

---
+++ Modulus Assignment/index.js
@@ -0,0 +1,12 @@
+let number = [___, ___]; //Boundary Numbers
+let skipCount = ___; //Number of skipCounting between each element of the sequence, used in FOR Loop
+let difference = number[] -xz number[]; //Difference, mandatory expectation
+let startingNumber = number[]; //Hint: used in the FOR Loop
+for (let i=0; i<=difference; i++){
+  even = (startingNumber += skipCount) % 2;
+  console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
+  console.log(number);
+  console.log(skipCount);
+  console.log(difference);
+  console.log(startingNumber);
+}
