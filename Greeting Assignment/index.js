87afffc58047139a8c54b73d081193e300357486
Yaqub-Ibrahim <37085705+Yaqub-Ibrahim@users.noreply.github.com>
2018-10-07 17:29:01 -0700

    completed first part of the greetning assignment

---
+++ Greeting Assignment/index.js
@@ -0,0 +1,8 @@
+let hour = 10; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+if (6<=hour<=12)
+ console.log('Good Morning');
+ else if
+  (13<=hour<=17)
+   console.log('Good Afternoon');
+ else
+  console.log('Good Evening');
