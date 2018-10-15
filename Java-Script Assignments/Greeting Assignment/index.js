ee048b0d93d615586a8e9190c3cda56fae7c041f
Yaqub-Ibrahim <37085705+Yaqub-Ibrahim@users.noreply.github.com>
2018-10-15 13:58:42 -0600

    update

--- Java-Script Assignments/Greeting Assignment/index.js
+++ Java-Script Assignments/Greeting Assignment/index.js
@@ -1,8 +1,10 @@
 let hour = 17; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
-if (6<=hour<=12)
+if (hour=6)
+if (hour<=12)
  console.log("It is", hour, "O'clock. Good morning!");
  else if
-  (13<=hour<=17)
+  (hour=13)
+  (hour<=17)
    console.log("It is", hour, "O'clock. Good afternoon!")
- else
-   console.log("It is", hour, "O'clock. Good evening!")
+   else
+     console.log("It is", hour, "O'clock. Good evening!")
